f3101e0bf858f57f355dda11d0c85d194e8065f2
KidDarek <madanotsirk@gmail.com>
2022-11-17 15:09:10 +0100

    Product page + Banner mid section back

--- src/pages/main-page/banner/Banner.jsx
+++ src/pages/main-page/banner/Banner.jsx
@@ -2,7 +2,16 @@ import React, {useState} from "react";
 import evoButtonL from "./evoButtonL.png"
 import evoButton from "./evoButton.png"
 import { styled } from "@mui/material";
-//import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+
+const StyledBannerDiv = styled("div")({
+  display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: '100%',
+    
+})
 
 
 const StyledLeftButton = styled("button")({
@@ -27,14 +36,14 @@ const Banner1 =({
   border: "0",
   height: "400px",
   width: "100%",
-  backgroundColor: "#00cc99"
+  backgroundImage: "linear-gradient(to right,#ff0055, #0066ff, #00cc99)",
 });
 
 const Banner2 =({
   border: "0",
   height: "400px",
   width: "100%",
-  backgroundColor: "#ff0066"
+  backgroundImage: "linear-gradient(to right, #00cc99, #0066ff, #ff0055)",
 });
 
 
@@ -43,26 +52,30 @@ const currentBanner = [Banner1,Banner2]
 const SetNewBanner = () => {
   const [bannerIndex, setIndex] = useState(0)
 
- // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  //const navigateToProductPage = () => {
- //   navigate("/Product");
-//  };
+  const navigateToProductPage = () => {
+  navigate("/Product");
+  };
 
   return(
-    <div style={currentBanner[bannerIndex % currentBanner.length]}>
-    <StyledLeftButton onClick={() => setIndex(bannerIndex - 1)}>
-    <img src={evoButtonL}
-    alt="Bal"
-    width={"60px"}/>
-    </StyledLeftButton>
-
-    <StyledRightButton onClick={() => setIndex(bannerIndex + 1)}>
-    <img src={evoButton}
-    alt="Jobb"
-    width={"60px"}/>
-    </StyledRightButton>
+   
+    <div style={currentBanner[Math.abs(bannerIndex % currentBanner.length)]}>
+      <StyledBannerDiv>
+      <StyledLeftButton onClick={() => setIndex(bannerIndex - 1)}>
+        <img src={evoButtonL}
+          alt="Bal"
+          width={"60px"} />
+      </StyledLeftButton>
+      <button style={{width: "90%", height: "100%", backgroundColor: "transparent" , border: "0"}} onClick={navigateToProductPage}></button>
+      <StyledRightButton onClick={() => setIndex(bannerIndex + 1)}>
+        <img src={evoButton}
+          alt="Jobb"
+          width={"60px"} />
+      </StyledRightButton>
+      </StyledBannerDiv>
     </div>
+
   )
 }
 
--- src/pages/product-page/ProductPage.jsx
+++ src/pages/product-page/ProductPage.jsx
@@ -1,15 +1,42 @@
+import styled from '@emotion/styled'
 import React from 'react'
 
+const StyledPageDiv = styled("div")({
+  display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: '100%',
+    padding: "25px",
+    backgroundImage: "linear-gradient(to right, #00cc99, #0066ff, #ff0055)",
+    
+})
+
+const StyledInfoDiv = styled("div")({
+  width: "50%",
+  alignItems: "left",
+  marginLeft: "25px",
+  justifyContent: "center",
+  position: "top",
+})
 
 
 const ProductPage = (props) => {
   return (
     <>
-    <div><img src="https://i.imgur.com/sJWUwiO.jpeg" alt="kep" height="500px" frame="true" /></div>
-    <div>ProductPage</div>
-    <div>ProductPage</div>
-    <div>ProductPage</div>
+    <StyledPageDiv>
+      <div><img src="https://i.imgur.com/sJWUwiO.jpeg" alt="kep" height="500px" frame="true" /></div>
+      <StyledInfoDiv>
+        <div>Név</div>
+        <div>Ár</div>
+        <div>Szín</div>
+        <div>Méret</div>
+      </StyledInfoDiv>
+    </StyledPageDiv>
+    <StyledPageDiv>
+      <button>Nem tom ide mi kell</button>
+    </StyledPageDiv>
     </>
+    
   )
 }
 
