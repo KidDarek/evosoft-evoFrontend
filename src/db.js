9fb2b5ff77ad1bc415c8761711719de0b44d3199
iklevente <ilyokovacslevente@gmail.com>
2022-11-23 19:47:49 +0100

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,23 +1,91 @@
 export const products = [
   {
+    id: 0,
     title: "Wooden PC",
     imageUri: "https://i.imgur.com/sJWUwiO.jpeg",
-    body: "Pray it won't catch on fire!",
+    body: "Superior gaming pc!",
     price: "$1000",
-    tag: "#computer"
+    category: "Computers",
+    tag: "[computer, gamer, electronics, IT]"
   },
   {
-    title: "iPhone 14",
-    imageUri: "https://imageio.forbes.com/specials-images/imageserve/627fa3b6a736222d2161069c/Apple--iPhone-14--iPhone-14-Pro--iPhone-14-Max--iPhone-14-Pro-Max--new-iPhone-/0x0.jpg?format=jpg&crop=1835,1375,x402,y49,safe&width=960",
-    body: "No innovation, as always!",
-    price: "$9999",
-    tag: "#smartphone"
+    id: 1,
+    title: "Microsoft Surface Pro 8 13''",
+    imageUri: "https://assets.mmsrg.com/isr/166325/c1/-/ASSET_MMS_91175301/mobile_786_587_png/MICROSOFT-Surface-Pro-8-13%22-256GB-WiFi-Ez%C3%BCst-Tablet-%288PQ-00005%29",
+    body: "An excellent windows laptop",
+    price: "$1000",
+    category: "Computers",
+    tag: "[computer, laptop, electronics, IT, Microsoft]"
   },
-  {},
+  {
+    id: 2,
+    title: "Apple Macbook M1 Pro",
+    imageUri: "https://cdn.vox-cdn.com/thumbor/8DJCNwon2vOPJm9K3H31NIugDxU=/0x0:2040x1360/2040x1360/filters:focal(1020x680:1021x681)/cdn.vox-cdn.com/uploads/chorus_asset/file/22046476/vpavic_4291_20201113_0366.0.jpg",
+    body: "An excellent ARM powered macbook",
+    price: "$1000",
+    category: "Computers",
+    tag: "[computer, laptop, electronics, IT, Apple, ARM, Mac]"
+  },
+
+  {
+    id: 3,
+    title: "Apple iPhone 14",
+    imageUri: "https://imageio.forbes.com/specials-images/imageserve/627fa3b6a736222d2161069c/Apple--iPhone-14--iPhone-14-Pro--iPhone-14-Max--iPhone-14-Pro-Max--new-iPhone-/0x0.jpg?format=jpg&crop=1835,1375,x402,y49,safe&width=960",
+    body: "With the latest dynamic island!",
+    price: "$8999",
+    category: "Smartphones",
+    tag: "[smartphone, electronics, Apple, iOS]"
+  },
+  {
+    id: 4,
+    title: "Samsung Galaxy S22 Ultra",
+    imageUri: "https://images.samsung.com/is/image/samsung/p6pim/ph/sm-s908elbgphl/gallery/ph-galaxy-s22-ultra-s908-410003-sm-s908elbgphl-533543999",
+    body: "The latest Samsung flagship with 108MPX camera",
+    price: "$9999",
+    category: "Smartphones",
+    tag: "[smartphone, electronics, Samsung, Android]"
+  },
+  {
+    id: 5,
+    title: "Xiaomi 12 Pro",
+    imageUri: "https://www.notebookcheck-hu.com/uploads/tx_nbc2/4_to_3_Teaser_Xiaomi_12_Pro.jpg",
+    body: "High quality for affordable price. Try the latest Xiaomi now!",
+    price: "$999",
+    category: "Smartphones",
+    tag: "[smartphone, electronics, Xiaomi, Android]"
+  },
+
+  {
+    id: 6,
+    title: "JBL Quantum One",
+    imageUri: "https://technuovo.com/wp-content/uploads/2020/11/jbl-quantum-one-gaming-headset-7.jpg",
+    body: "A gaming headset with immersive surround sound...",
+    price: "$1000",
+    category: "Computer accessories",
+    tag: "[headset, gamer, electronics, IT, JBL]"
+  },
+  {
+    id: 7,
+    title: "Razer DeathStalker V2",
+    imageUri: "https://www.pcworld.com/wp-content/uploads/2022/07/razer-deathstalker-tkl.jpg?quality=50&strip=all&w=1024",
+    body: "Featuring all-new low-profile switches within a lean, durable casing, experience ultra-responsive gaming designed for long-lasting ergonomic use.",
+    price: "$2000",
+    category: "Computer accessories",
+    tag: "[keyboard, gamer, electronics, IT, Razer]"
+  },
+  {
+    id: 8,
+    title: "Corsair Sabre RGB Pro",
+    imageUri: "https://cdn.pocket-lint.com/r/s/970x/assets/images/158480-gadgets-review-corsair-sabre-rgb-pro-review-less-rgb-more-freedom-image2-h1iyr4c7xq.jpg",
+    body: "The CORSAIR SABRE RGB PRO Gaming Mouse is designed with feedback from esports pros, weighing an ultra-light 74g...",
+    price: "$2000",
+    category: "Computer accessories",
+    tag: "[mouse, gamer, electronics, IT, Corsair]"
+  }
 ];
 export const users = [
   {
     email:"teszt",
-    password:"teszt",
+    password:"teszt"
   }
 ]
