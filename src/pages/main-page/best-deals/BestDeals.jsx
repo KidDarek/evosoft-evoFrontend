7586dafd5e9a65ab5480c9540b5f786ede94e710
iklevente <ilyokovacslevente@gmail.com>
2022-11-23 23:06:14 +0100

    BestDeals now picks two random cetegories to list products from db

--- src/pages/main-page/best-deals/BestDeals.jsx
+++ src/pages/main-page/best-deals/BestDeals.jsx
@@ -9,17 +9,34 @@ const StyledPadding = styled("div")({
   paddingRight: "9rem",
 });
 
-const ReturnProductsByCategory = () => {
+const ReturnTwoRandomProductByCategory = () => {
 
+  // Filtering all products categories and putting it in an array
   let filtered = [];
-  
   products.map((product) => (
     !filtered.includes(product.category) ?
     filtered.push(product.category) : null
   ))
+  
+  // Choosing two random category which can not be a duplicate
+  let twoRandom = [];
+  let randomNumber = null;
+  for (let i = 0; i < filtered.length - 1; i++)
+  {
+    if (twoRandom.includes(filtered[randomNumber]) || randomNumber === null)
+    {
+      i--;
+      randomNumber = Math.floor(Math.random() * ((filtered.length - 1) + 1));
+    }
+    else
+    {
+      twoRandom.push(filtered[randomNumber]);
+    }
+  }
 
+  // Returning category names with it's assigned items
   return (
-    filtered.map((category) => (
+    twoRandom.map((category) => (
       <>
       <h2>{category}:</h2>
       <hr style={{marginBottom: "2rem"}}/>
@@ -41,14 +58,13 @@ const ReturnProductsByCategory = () => {
       </>
     ))
   )
-
 }
 
 const BestDealsPage = () => {
   return (
     <>
       <StyledPadding>
-      <ReturnProductsByCategory />
+      <ReturnTwoRandomProductByCategory />
       </StyledPadding>
     </>
   );
