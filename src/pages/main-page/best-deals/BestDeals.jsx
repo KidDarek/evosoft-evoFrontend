038e684276235667eda0122f8b414e50949c3474
iklevente <ilyokovacslevente@gmail.com>
2022-11-23 22:34:18 +0100

    refactor best deals component so it dinamically loads the data from db

--- src/pages/main-page/best-deals/BestDeals.jsx
+++ src/pages/main-page/best-deals/BestDeals.jsx
@@ -3,73 +3,52 @@ import React from "react";
 import Card from "./Card";
 import { styled } from "@mui/material";
 import { products } from "../../../db";
-//import linq from "linq";
-
-const CardFiller = (props) => {
-  return (
-    <Grid item xs="auto" md="auto">
-      <Card
-        title={props.title}
-        imageUri={props.imageUri}
-        body={props.body}
-        price={props.price}
-      />
-    </Grid>
-  );
-};
-
-const RequestDatabase = products.forEach((product) => {
-  <CardFiller
-    title={product.title}
-    imageUri={product.imageUri}
-    body={product.body}
-    price={product.price}
-  />;
-});
-
-// };
 
 const StyledPadding = styled("div")({
-  paddingLeft: "3rem",
-  paddingRight: "3rem",
+  paddingLeft: "9rem",
+  paddingRight: "9rem",
 });
 
-const BestDealsPage = () => {
-  return (
-    <>
-      <StyledPadding>
-        <h2>Computers:</h2>
+const ReturnProductsByCategory = () => {
 
-        <Grid container spacing={10} justifyContent="center">
-          {/* <CardFiller 
-          title = "Wooden PC"
-          imageUri = "https://i.imgur.com/sJWUwiO.jpeg"
-          body = "Pray it won't catch on fire!"
-          price = "$1000"
-          />
-          <CardFiller 
-          title = "Wooden PC"
-          imageUri = "https://i.imgur.com/sJWUwiO.jpeg"
-          body = "Pray it won't catch on fire!"
-          price = "$1000"
-          />
-          <CardFiller 
-          title = "Wooden PC"
-          imageUri = "https://i.imgur.com/sJWUwiO.jpeg"
-          body = "Pray it won't catch on fire!"
-          price = "$1000"
-        /> */}
-          {/* <RequestDatabase /> */}
-          {products.map((product, index) => (
-            <CardFiller
-              key={index}
+  let filtered = [];
+  
+  products.map((product) => (
+    !filtered.includes(product.category) ?
+    filtered.push(product.category) : null
+  ))
+
+  return (
+    filtered.map((category) => (
+      <>
+      <h2>{category}:</h2>
+      <hr style={{marginBottom: "2rem"}}/>
+      <Grid container spacing={30} justifyContent="center">
+      {
+        products.map((product) => (
+          product.category === category ?
+          <Grid item xs="auto" md="auto" key={product.id}>
+            <Card
               title={product.title}
               imageUri={product.imageUri}
               body={product.body}
               price={product.price}
             />
-          ))}
-        </Grid>
+          </Grid> : null
+        ))
+      }
+      </Grid>
+      </>
+    ))
+  )
+
+}
+
+const BestDealsPage = () => {
+  return (
+    <>
+      <StyledPadding>
+      <ReturnProductsByCategory />
       </StyledPadding>
     </>
   );
