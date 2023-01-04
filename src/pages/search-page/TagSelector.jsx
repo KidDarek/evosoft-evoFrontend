73d81b42b55b0dfc1943719a9081747eeedbd968
iklevente <ilyokovacslevente@gmail.com>
2023-01-04 23:21:15 +0100

    Update TagSelector.jsx

--- src/pages/search-page/TagSelector.jsx
+++ src/pages/search-page/TagSelector.jsx
@@ -1,24 +1,38 @@
-import { Checkbox } from '@mui/material'
-import React from 'react'
+import { Checkbox, filledInputClasses } from "@mui/material";
+import { Grid } from "@mui/material";
+import { products } from "../../db";
+import React, { Fragment } from "react";
+import { display } from "@mui/system";
+
+const ReturnTags = () => {
+  const tags = new Set();
+
+  products.forEach((item) => {
+    const itemTags = item.tag.slice(1, -1).split(", ");
+    itemTags.forEach((tag) => tags.add(tag));
+  });
+
+  const uniqueTags = [...tags];
+
+  return (
+    <div>
+      {uniqueTags.map((tag) => (
+        <div key={tag} style={{ display: "inline-block", margin: "10px" }}>
+          <input type="checkbox" id={tag} value={tag} />
+          <label htmlFor={tag}>{tag}</label>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const TagSelector = () => {
-    return (
-        <>
-            <div> Tags</div>
-            <div style={{ witdh: "200px" }}>
-                <text>Computer</text>
-                <Checkbox> tag 1</Checkbox>
-            </div>
-            <div style={{ witdh: "200px" }}>
-                <text>Phone</text>
-                <Checkbox> tag 1</Checkbox>
-            </div>
-            <div style={{ witdh: "200px" }}>
-                <text>Microphone</text>
-                <Checkbox> tag 1</Checkbox>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div>Tags:</div>
+      <ReturnTags></ReturnTags>
+    </>
+  );
+};
 
-export default TagSelector
+export default TagSelector;
