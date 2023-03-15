486d727a3dba29b6c765b9118c8026f87bf14450
HolczerVanda <90444495+HolczerVanda@users.noreply.github.com>
2023-03-15 17:08:24 +0100

    Profile page design (#2)
    Changed Profile page to be consistent with the rest of the site
    
    Co-authored-by: Holczer Vanda <vanda.holczer@prodhost.com>

--- src/pages/profile-page/ProfilePage.jsx
+++ src/pages/profile-page/ProfilePage.jsx
@@ -9,9 +9,7 @@ const StyledPageDiv = styled("div")({
   justifyContent: "center",
   height: "100%",
   padding: "10px 25px 10px 25px",
-  marginTop: "20px",
-  marginBottom: "15px",
-  gap: "25px",
+  backgroundColor: "#00EFB3",
 });
 
 const StyledTable = styled("table")({
@@ -19,41 +17,48 @@ const StyledTable = styled("table")({
   height: "128px",
   background: "#00cc99",
   borderRadius: "15px",
+  color: "white",
+  padding: "20px 20px 20px 24px",
+  fontWeight: 'bold'
 });
 
 const ProfilePage = () => {
   const params = useParams();
   const id = params.id;
   return (
-    <div>
+    <>
       <StyledPageDiv>
-        <Avatar
-          sx={{ width: 128, height: 128, bgcolor: "#ff0055", fontSize: 100 }}
-        >
-          {users[id - 1].name[0].toUpperCase()}
-        </Avatar>
         <StyledTable>
           <tbody>
             <tr>
-              <td>Name</td>
+              <td rowspan="5">
+                <Avatar
+                  sx={{ width: 128, height: 128, bgcolor: "#ff0055", fontSize: 100 }}
+                >
+                  {users[id - 1].name[0].toUpperCase()}
+                </Avatar>
+              </td>
+            </tr>
+            <tr>
+              <td>Name:</td>
               <td>{users[id - 1].name}</td>
             </tr>
             <tr>
-              <td>Email</td>
+              <td>Email:</td>
               <td>{users[id - 1].email}</td>
             </tr>
             <tr>
-              <td>Role</td>
+              <td>Role:</td>
               <td>{users[id - 1].role}</td>
             </tr>
             <tr>
-              <td>Status</td>
+              <td>Status:</td>
               <td>active</td>
             </tr>
           </tbody>
         </StyledTable>
       </StyledPageDiv>
-    </div>
+    </>
   );
 };
 
