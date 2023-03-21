039219309464781fa25e39a45bfa2218ae3ed5d8
KidDarek <90865592+KidDarek@users.noreply.github.com>
2023-03-21 19:08:15 +0100

    Profile page Design (#5)
    * Profile page design
    Changed Profile page to be consistent with the rest of the site
    
    * Profile Page Purchase History button
    
    * Animation on the Purchases in history
    
    ---------
    
    Co-authored-by: Holczer Vanda <vanda.holczer@prodhost.com>

--- src/pages/profile-page/ProfilePage.jsx
+++ src/pages/profile-page/ProfilePage.jsx
@@ -1,7 +1,8 @@
 import { Avatar, styled } from "@mui/material";
-import React from "react";
 import { useParams } from "react-router-dom";
 import { users } from "../../db";
+import React, { useState } from "react";
+import PurchaseHistoryButton from "./PurchaseHistoryButton";
 
 const StyledPageDiv = styled("div")({
   display: "flex",
@@ -11,6 +12,51 @@ const StyledPageDiv = styled("div")({
   padding: "10px 25px 10px 25px",
   backgroundColor: "#00EFB3",
 });
+
+const StyledBrightPurchaseHistoryDiv = styled("div")({
+  width: "100%",
+  height: "100%",
+  backgroundColor: "#00e6ac",
+  color: "white",
+  textAlign: "center",
+  paddingTop: "50px",
+  paddingBottom: "50px",
+  "@keyframes brightdivanimation": {
+    from: {
+      transform: "translateX(100%)",
+      opacity: "0",
+    },
+    to: {
+      transform: "translateX(0)",
+      opacity: "1",
+    },
+  },
+  animation: "brightdivanimation 0.5s 1 ease",
+  position: "static"
+})
+
+const StyledDarkPurchaseHistoryDiv = styled("div")({
+  width: "100%",
+  height: "100%",
+  backgroundColor: "#00b386",
+  color: "white",
+  textAlign: "center",
+  paddingTop: "50px",
+  paddingBottom: "50px",
+  "@keyframes darkdivanimation": {
+    from: {
+      transform: "translateX(-100%)",
+      opacity: "0",
+    },
+    to: {
+      transform: "translateX(0)",
+      opacity: "1",
+    },
+  },
+  animation: "darkdivanimation 0.5s 1 ease",
+  position: "static"
+})
+
 
 const StyledTable = styled("table")({
   width: "25%",
@@ -22,7 +68,15 @@ const StyledTable = styled("table")({
   fontWeight: 'bold'
 });
 
+const StyledPurchaseHistory = styled("div")({
+  width: "80%",
+  height: "100%",
+  backgroundColor: "#00cc99",
+})
+
 const ProfilePage = () => {
+  const [VisiblePurchaseHistory, setPurchaseHistory] = useState(false);
+
   const params = useParams();
   const id = params.id;
   return (
@@ -57,6 +111,16 @@ const ProfilePage = () => {
             </tr>
           </tbody>
         </StyledTable>
+      </StyledPageDiv>
+      <StyledPageDiv>
+        {!VisiblePurchaseHistory && <PurchaseHistoryButton setPurchaseHistory={setPurchaseHistory}></PurchaseHistoryButton>}
+        {VisiblePurchaseHistory &&
+          <StyledPurchaseHistory>
+            <StyledDarkPurchaseHistoryDiv> Product 1 </StyledDarkPurchaseHistoryDiv>
+            <StyledBrightPurchaseHistoryDiv> Product 2 </StyledBrightPurchaseHistoryDiv>
+            <StyledDarkPurchaseHistoryDiv> Product 3 </StyledDarkPurchaseHistoryDiv>
+            <StyledBrightPurchaseHistoryDiv> Product 4 </StyledBrightPurchaseHistoryDiv>
+          </StyledPurchaseHistory>}
       </StyledPageDiv>
     </>
   );
