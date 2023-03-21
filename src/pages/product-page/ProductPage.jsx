39f704e56adace126ba1d573e23016f744d735ab
iklevente <ilyokovacslevente@gmail.com>
2023-03-21 12:11:02 +0100

    Bugfix - Image exceeds grey margin

--- src/pages/product-page/ProductPage.jsx
+++ src/pages/product-page/ProductPage.jsx
@@ -16,10 +16,8 @@ const StyledPageDiv = styled("div")({
 
 const StyledInfoDiv = styled("div")({
   width: "60%",
-  alignItems: "center",
   display: "flex",
   justifyContent: "center",
-  height: "800px",
   border: "10px solid grey",
   backgroundColor: "#00cc99",
 });
@@ -49,13 +47,14 @@ const StyledInfoDivText2 = styled("div")({
 });
 
 const StyledImage = styled("img")({
-  height: "100%",
-  width: "100%",
-  frame: "true",
+  height: "auto",
+  width: "auto",
+  maxWidth: "100%",
+  maxHeight: "100%",
   display: "block",
   marginLeft: "auto",
   marginRight: "auto",
-})
+});
 
 const BasicTheme = createTheme({
   palette: {
@@ -75,69 +74,64 @@ const BasicTheme = createTheme({
 });
 
 const AddItemToShoppingCart = (id) => {
-  let shoppingItems = JSON.parse(localStorage.getItem("shoppingItems")) === null ? [] : JSON.parse(localStorage.getItem("shoppingItems"));
+  let shoppingItems =
+    JSON.parse(localStorage.getItem("shoppingItems")) === null
+      ? []
+      : JSON.parse(localStorage.getItem("shoppingItems"));
   const itemQuantityInput = document.getElementById("item-quantity");
   const quantity = parseInt(itemQuantityInput.value);
   const price = products[id].price;
   if (quantity === 0) {
     return;
   }
-  id = parseInt(id)
+  id = parseInt(id);
   for (let index = 0; index < shoppingItems.length; index++) {
     if (shoppingItems[index].id === id) {
       return;
     }
   }
   const shoppingItem = { id, quantity, price };
-  shoppingItems.push(shoppingItem)
+  shoppingItems.push(shoppingItem);
   localStorage.setItem("shoppingItems", JSON.stringify(shoppingItems));
-}
+};
 
 const ProductPage = (props) => {
   const [value, setValue] = React.useState("1");
 
   let accountRole = users[0].role;
-  console.log(accountRole)
+  console.log(accountRole);
 
   const params = useParams();
-  const id = params.id
+  const id = params.id;
   return (
     <>
       <ThemeProvider theme={BasicTheme}>
         <StyledPageDiv>
           <StyledInfoDiv>
             <div>
-              <StyledImage
-                src={products[id].imageUri}
-                alt="kep"
-
-              />
+              <StyledImage src={products[id].imageUri} alt="kep" />
             </div>
           </StyledInfoDiv>
           <StyledInfoDivText>
             <div>
-              <h2 style={{ color: "white" }}>
-                Product name:
-              </h2>
+              <h2 style={{ color: "white" }}>Product name:</h2>
               <div style={{ color: "white" }}> {products[id].title}</div>
-              <h2 style={{ color: "white" }}>
-                Price:
-              </h2>
+              <h2 style={{ color: "white" }}>Price:</h2>
               <div style={{ color: "white" }}> {products[id].price}</div>
-              <h2 style={{ color: "white" }}>
-                Category:
-              </h2>
+              <h2 style={{ color: "white" }}>Category:</h2>
               <div style={{ color: "white" }}> {products[id].category}</div>
-              <h2 style={{ color: "white" }}>
-                Tags:
-              </h2>
+              <h2 style={{ color: "white" }}>Tags:</h2>
               <div style={{ color: "white" }}>
-                {products[id].tag.map((i) =>
-                  i + ", "
-                )}
+                {products[id].tag.map((i) => i + ", ")}
               </div>
               <div style={{ paddingTop: "20px" }}>
-                <MUIButton variant="contained" onClick={() => AddItemToShoppingCart(id)}> Add item to cart </MUIButton>
+                <MUIButton
+                  variant="contained"
+                  onClick={() => AddItemToShoppingCart(id)}
+                >
+                  {" "}
+                  Add item to cart{" "}
+                </MUIButton>
                 <TextField
                   focused
                   margin="dense"
@@ -163,7 +157,6 @@ const ProductPage = (props) => {
                 />
               </div>
             </div>
-
           </StyledInfoDivText>
         </StyledPageDiv>
         <StyledPageDiv>
