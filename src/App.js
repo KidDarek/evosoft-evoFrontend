c1bee28c8130f30344ab9c96f30303c98dcad449
HolczerVanda <vanda.holczer@gmail.com>
2023-04-23 13:47:37 +0200

    Implemented an Answer question page, where user questions can be answered or deleted.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { styled } from "@mui/material";
 import SearchPage from "./pages/search-page/SearchPage";
 import ShopPage from "./pages/shop-page/ShopPage";
 import CheckoutPage from "./pages/checkout-page/CheckoutPage";
+import AnswerQuestionPage from "./pages/answer-question-page/AnswerQuestionPage";
 
 const StyledFontDiv = styled("div")({
   fontFamily: "Roboto",
@@ -30,6 +31,7 @@ function App() {
             <Route path="/Search" element={<SearchPage />} />
             <Route path="/Shop" element={<ShopPage />} />
             <Route path="/Checkout" element={<CheckoutPage />} />
+            <Route path="/AnswerQuestion" element={<AnswerQuestionPage />} />
             <Route path="/Profile:id" element={<ProfilePage />} />
             <Route path="/Product/:id" element={<ProductPage />} />
           </Routes>
