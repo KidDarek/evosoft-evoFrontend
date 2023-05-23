301b92716b01f3448cb1008ce6e3521e4634879e
KidDarek <madanotsirk@gmail.com>
2023-05-23 18:23:45 +0200

    Update SimilarProdCard.jsx

--- src/pages/product-page/SimilarProdCard.jsx
+++ src/pages/product-page/SimilarProdCard.jsx
@@ -1,5 +1,8 @@
 import styled from "@emotion/styled";
 import React, { Fragment, useContext } from "react";
+import {
+    ProductContext,
+} from "../../../context-providers/ProductContext";
 
 const StyledProdCard = styled("div")({
     height: "auto",
