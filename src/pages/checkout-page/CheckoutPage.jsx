19022fb00351f2511d6003b8a74df69a363bba58
iklevente <ilyokovacslevente@gmail.com>
2023-06-06 19:37:33 +0200

    Ask for location at checkout page

--- src/pages/checkout-page/CheckoutPage.jsx
+++ src/pages/checkout-page/CheckoutPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Button, styled } from "@mui/material";
 import {
@@ -48,6 +48,57 @@ const CheckoutPageInside = () => {
   const location = useLocation();
   const grandTotal = location.state?.grandTotal;
 
+  const [geolocation, setGeolocation] = useState(null);
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setGeolocation({ latitude, longitude });
+        },
+        (error) => {
+          console.error("Error getting geolocation:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  const getAddressFromGeolocation = async (latitude, longitude) => {
+    try {
+      const response = await fetch(
+        `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        const { address } = data;
+        // Update the address fields with the retrieved data
+        document.getElementById("postcode").value = address?.postcode || "";
+        document.getElementById("road").value = address?.road || "";
+
+        const cityAndCountry = `${address?.city || ""}, ${
+          address?.country || ""
+        }`;
+        document.getElementById("city").value = cityAndCountry;
+
+        const roadAndHouseNumber = `${address?.road || ""} ${
+          address?.house_number || ""
+        }`;
+        document.getElementById("road").value = roadAndHouseNumber;
+      } else {
+        console.error("Error retrieving address from geolocation.");
+      }
+    } catch (error) {
+      console.error("Error retrieving address from geolocation:", error);
+    }
+  };
+  useEffect(() => {
+    if (geolocation) {
+      getAddressFromGeolocation(geolocation.latitude, geolocation.longitude);
+    }
+  }, [geolocation]);
+
   return (
     <>
       <StyledPageDiv>
@@ -76,30 +127,18 @@ const CheckoutPageInside = () => {
             </tr>
             <tr>
               <td>
-                <StyledInput
-                  type="text"
-                  placeholder="First name"
-                />
+                <StyledInput type="text" placeholder="First name" />
               </td>
               <td>
-                <StyledInput
-                  type="text"
-                  placeholder="Last name"
-                />
+                <StyledInput type="text" placeholder="Last name" />
               </td>
             </tr>
             <tr>
               <td>
-                <StyledInput
-                  type="text"
-                  placeholder="Email"
-                />
+                <StyledInput type="text" placeholder="Email" />
               </td>
               <td>
-                <StyledInput
-                  type="tel"
-                  placeholder="Phone number"
-                />
+                <StyledInput type="tel" placeholder="Phone number" />
               </td>
             </tr>
             <tr>
@@ -107,13 +146,11 @@ const CheckoutPageInside = () => {
                 <StyledInput
                   type="text"
                   placeholder="Post code"
+                  id="postcode"
                 />
               </td>
               <td>
-                <StyledInput
-                  type="text"
-                  placeholder="City"
-                />
+                <StyledInput type="text" placeholder="City" id="city" />
               </td>
             </tr>
             <tr>
@@ -122,6 +159,7 @@ const CheckoutPageInside = () => {
                   type="text"
                   placeholder="Street, house number"
                   size="47"
+                  id="road"
                 />
               </td>
             </tr>
@@ -135,11 +173,7 @@ const CheckoutPageInside = () => {
               </tr>
               <tr>
                 <td colSpan="2">
-                  <input
-                    type="checkbox"
-                    checked="false"
-                    name="acceptPolicy"
-                  />
+                  <input type="checkbox" checked="false" name="acceptPolicy" />
                   <StyledLabel for="acceptPolicy">
                     I would like to get emails about the best deals.
                   </StyledLabel>
@@ -147,11 +181,7 @@ const CheckoutPageInside = () => {
               </tr>
               <tr>
                 <td colSpan="2">
-                  <input
-                    type="checkbox"
-                    checked="false"
-                    name="acceptPolicy"
-                  />
+                  <input type="checkbox" checked="false" name="acceptPolicy" />
                   <StyledLabel for="acceptPolicy">
                     I have read and accepted that my order involves a payment
                     obligation, with ordering I agree to Terms of Service and
@@ -165,11 +195,7 @@ const CheckoutPageInside = () => {
               </tr>
               <tr>
                 <td colSpan="3" align="center">
-                  <Button
-                    variant="contained"
-                    color="green"
-                    sx={{ width: 325 }}
-                  >
+                  <Button variant="contained" color="red" sx={{ width: 325 }}>
                     Order
                   </Button>
                 </td>
